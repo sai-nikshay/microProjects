66c26713eb78326016b501195817b8bac376c48f
sai-nikshay <55012040+sai-nikshay@users.noreply.github.com>
2020-10-03 11:24:15 +0530

    Update project1.js

--- microproject_1/project1.js
+++ microproject_1/project1.js
@@ -2,6 +2,7 @@ var express= require('express');
 var bodyParser = require('body-parser');
 var middleware=require('./middleware');
 var server=require('./server');
+const { json } = require('body-parser');
 var app=express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,59 +12,61 @@ const databasename='proj1';
 const ventname ="ventilators";
 const hospitalsc='hospitals';
 let db
-MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true},(err,client)=>{
+MongoClient.connect(url,{useUnifiedTopology: true, useNewUrlParser: true},(err,client)=>{
     if(err) return console.log(err);
     db=client.db(databasename);
-app.get('/hospitaldetails',middleware.checkToken, (req, res) => {
-    var hospitaldetails=db.collection(hospitalsc).find().toArray().then(result => res.json(result));
+app.get('/hospdata',middleware.checkToken, (req, res) => {
+    db.collection(hospitalsc).find().toArray().then(result => res.json(result));
     });
-app.get('/ventilatordetails',middleware.checkToken, (req, res) => {
-    var ventilatordetails=db.collection(ventname).find().toArray().then(result => res.json(result));
+app.get('/ventdata',middleware.checkToken, (req, res) => {
+    db.collection(ventname).find().toArray().then(result => res.json(result));
     });
-app.post('/hospitalname1',middleware.checkToken, (req, res) => {
+app.post('/hn1',middleware.checkToken, (req, res) => {
     var hon=req.body.hon;
-    var vs=req.body.vs;
     console.log(hon);
-    var ventilatordetails=db.collection(ventname).find({"name":new RegExp(hon,'i')}).toArray().then(result => res.json(result));
+    db.collection(ventname).find({"name":new RegExp(hon,'i')}).toArray().then(result => res.json(result));
     });
-    app.post('/hospitalname2',middleware.checkToken, (req, res) => {
-        var hon=req.body.hon;
-        console.log(hon);
-        var ventilatordetails=db.collection(hospitalsc).find({"name":new RegExp(hon,'i')}).toArray().then(result => res.json(result));  
-        });  
-app.post('/status',middleware.checkToken, (req, res) => {
+app.post('/hn2',middleware.checkToken, (req, res) => {
+    var hon=req.body.hon;
+    console.log(hon);
+    db.collection(hospitalsc).find({"name":new RegExp(hon,'i')}).toArray().then(result => res.json(result));  
+    });  
+app.post('/Vst',middleware.checkToken, (req, res) => {
     var vs=req.body.vs;
-    var ventilatorstatus=db.collection(ventname).find({"status":vs}).toArray().then(result => res.json(result));  
+    db.collection(ventname).find({"status":vs}).toArray().then(result => res.json(result));  
     });
-       app.put('/update',middleware.checkToken,(req,res)=>{
+app.put('/upd',middleware.checkToken,(req,res)=>{
         db.collection(ventname, function (err, collection) {
         
             collection.update({"ventid":req.body.ventid}, { $set: {"status" : req.body.vs} },
                                                          function(err, result){
                                                                     if(err) throw err;    
+                                                                    res.json("updated");
                                                                     console.log('Document Updated Successfully');
                                                             });
             });
         });
-        app.post('/add',middleware.checkToken,(req,res)=>
+app.post('/add',middleware.checkToken,(req,res)=>
+    {
+        db.collection(ventname,function(err,collection)
         {
-            db.collection(ventname,function(err,collection)
-            {
-                collection.insert({ "hid" :req.body.nid,
-                "ventid" : req.body.nvid,
-                "status" : req.body.ns,
-                "name" : req.body.nn})
-            });
+            collection.insert({ "hid" :req.body.hid,
+            "ventid" : req.body.vid,
+            "status" : req.body.vs,
+            "name" : req.body.hn})
         });
-        app.delete('/remove',middleware.checkToken,(req,res)=>
-        {
-            db.collection(ventname,function(err,collection)
-            {
-                collection.remove({"ventilatorId":req.body.ventid});
-            });
-        });
+        res.json("Added");
+    });
+app.delete('/del',middleware.checkToken,(req,res)=>
+{
+    db.collection(ventname,function(err,collection)
+    {
+    collection.remove({"ventid":req.body.vid});
+    });
+    res.json("deleted");
+});
 
-    app.listen(3006);
+    app.listen(3007);
     
 
 
